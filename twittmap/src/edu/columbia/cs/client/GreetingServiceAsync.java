ec8b6111ead68e55d66e339d00d747d61b65f268
sg3163 <sg3163@columbia.edu>
2014-04-09 23:47:02 -0400

    After adding test module

---
+++ twittmap/src/edu/columbia/cs/client/GreetingServiceAsync.java
@@ -0,0 +1,11 @@
+package edu.columbia.cs.client;
+
+import com.google.gwt.user.client.rpc.AsyncCallback;
+
+/**
+ * The async counterpart of <code>GreetingService</code>.
+ */
+public interface GreetingServiceAsync {
+	void greetServer(String input, AsyncCallback<String> callback)
+			throws IllegalArgumentException;
+}
